892fd0251a11891e76e187c86cc7ca925ab1c8c0
sarahtartaruga <fannyFlye@gmx.de>
2020-05-05 20:56:46 +0200

    slide animation

--- src/Book.js
+++ src/Book.js
@@ -7,75 +7,70 @@ class Book extends React.Component {
     this.images = props.images;
     this.totalPages = props.images.length;
     this.selected = 0;
-    // this.state = {
-    //   selected: 0,
-    // };
-    // this.handleSelectedChange = this.handleSelectedChange.bind(this);
-    // this.previous = this.previous.bind(this);
+    this.handleSelectedChange = this.handleSelectedChange.bind(this);
+    this.previous = this.previous.bind(this);
     this.next = this.next.bind(this);
-    this.wrapperRef = React.createRef();
+    this.imgRefs = [];
+    for (let i = 0; i < this.totalPages; i++) {
+      this.imgRefs.push(React.createRef());
+    }
   }
 
-  // handleSelectedChange(selected) {
-  //   this.setState({ selected });
-  // }
-  //
-  // previous() {
-  //   this.setState((state) => ({
-  //     selected: state.selected - 1,
-  //   }));
-  // }
+  handleSelectedChange(selected) {
+    this.selected = selected;
+  }
+
+  previous() {
+    this.selected = this.selected - 1;
+  }
 
   next() {
-    const wrapper = this.wrapperRef.current;
-    wrapper.classList.toggle('is-open');
-    // this.setState((state) => ({
-    //   selected: state.selected + 1,
-    // }));
+    if (this.selected < this.totalPages - 1) {
+      const img = this.imgRefs[this.selected].current;
+      img.classList.toggle("Page-image--remove");
+    }
     this.selected = this.selected + 1;
-    this.forceUpdate();
+    //this.forceUpdate();
   }
 
   createPages() {
-      let div = [];
-      div.push(<div className={"Page"}></div>);
-      for (let i = this.selected; i < this.totalPages; i++) {
-        let divStyle = {
-          zIndex: this.totalPages - i,
-        };
-        div.push(
-          <img
-            className={"Page-image Page-image-" + i}
-            src={this.images[i]}
-            alt={"image-" + i}
-            style={divStyle}
-          ></img>
-        );
-      }
+    let div = [];
+    div.push(<div className={"Page"}></div>);
+    for (let i = this.selected; i < this.totalPages; i++) {
+      let divStyle = {
+        zIndex: this.totalPages - i,
+      };
+      div.push(
+        <img
+          ref={this.imgRefs[i]}
+          className={"Page-image"}
+          src={this.images[i]}
+          alt={"image-" + i}
+          style={divStyle}
+        ></img>
+      );
+    }
 
-      if (this.selected >= this.totalPages) {
-        let divStyle = {
-          zIndex: this.totalPages,
-        };
-        div.push(
-          <img
-            className={"Page-image Page-image-" + this.totalPages}
-            src={this.images[this.totalPages-1]}
-            alt={"image-" + this.totalPages}
-            style={divStyle}
-          ></img>
-        );
-      }
-      return div;
+    if (this.selected >= this.totalPages) {
+      let divStyle = {
+        zIndex: this.totalPages,
+      };
+      div.push(
+        <img
+          className={"Page-image Page-image-" + this.totalPages}
+          src={this.images[this.totalPages - 1]}
+          alt={"image-" + this.totalPages}
+          style={divStyle}
+        ></img>
+      );
+    }
+    return div;
   }
 
   render() {
     return (
       <div className="Book" onClick={this.next.bind(this)}>
-      <div ref={this.wrapperRef} className="wrapper">
-
         {this.createPages()}
-      </div>
       </div>
     );
   }
