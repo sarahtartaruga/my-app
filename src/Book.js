5083028e15d6ce03025d1687a358f1914e3d36a3
sarahtartaruga <fannyFlye@gmx.de>
2020-05-05 17:35:08 +0200

    starting with image stack

--- src/Book.js
+++ src/Book.js
@@ -30,50 +30,26 @@ class Book extends React.Component {
     }));
   }
 
-  // creates pages i with CSS classes Page-i and image src from props array
-  // TODO: better exclude Page as a single class?
   createPages() {
-    let pages = [];
-
+    let div = [];
+    div.push(<div className={"Page"}></div>);
     for (let i = 0; i < this.totalPages; i++) {
-      // default back page
-      let pageStyle = {
-        background: '#f44336',
-      };
-      // other pages: have following page as background
-      if (i < this.totalPages-1) {
-        pageStyle = {
-          backgroundImage: 'url(' + this.images[i+1] + ')',
-          backgroundRepeat: 'no-repeat',
-
-        };
-      };
-
-      pages.push(
-        <div className={"Page Page-" + i} style={pageStyle} key={i}>
-          <img className={"Page-image"} src={this.images[i]} alt={"image-" + i}></img>
-        </div>
+      div.push(
+        <img
+          className={"Page-image Page-image-" + i}
+          src={this.images[i]}
+          alt={"image-" + i}
+        ></img>
       );
     }
-    return pages;
+
+    return div;
   }
 
   render() {
     return (
       <div className="Book">
-      <div className="BookContainer">
-          {this.createPages()}
-        <div className="Space" id="ButtonSection">
-        <button className="DirectionButton" onClick={this.previous} disabled={!this.state.selected}>
-          Previous
-        </button>
-        <button className="DirectionButton"
-          onClick={this.next}
-          disabled={this.state.selected + 1 === this.totalPages}
-        >
-          Next
-        </button></div>
-        </div>
+        <div className="BookContainer">{this.createPages()}</div>
       </div>
     );
   }
